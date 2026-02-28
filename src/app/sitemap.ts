8a732b44a2e29b636b9b3c3b25e65252f91a513f
Can <yerkan884@gmail.com>
2026-02-28 21:23:11 +0300

    bug fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { db } from "@/server/db";
 
+export const dynamic = "force-dynamic";
+
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://qrmenu.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
